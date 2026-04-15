87c3ffa6c1732111f894c6c9b161a2dda208189b
brayanson1 <130611718+brayanson1@users.noreply.github.com>
2026-04-15 14:42:27 -0500

    html y css erminado

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let edad = 30;
+const esMayorEdad = true;
+const nombre = "Yunior Dennis";
+const precio = 99.99;
+
+console.log(nombre, precio, edad);
+
+edad += 5;
+
+console.log(edad);
+console.log("suma: ", 5 + 5);
+//&//
+if(edad >= 18 ){
+    console.log("Mayor de edad");
+}else{
+    console.log("Menor de edad");
+}
+const frutas = ["manzana", "pera", "naranja", "fresa"];
+frutas.map((fruta)=>{
+    console.log(fruta);
+
+});
